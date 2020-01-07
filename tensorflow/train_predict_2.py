b3ef89b2a19754122c58a13918ef1c4821f33d1e
bjornm82 <bjmooijekind@gmail.com>
2020-01-07 14:02:16 +0100

    Changed to other type

--- tensorflow/train_predict_2.py
+++ tensorflow/train_predict_2.py
@@ -144,11 +144,13 @@ def main(argv):
             'PetalWidth': tf.Variable([], dtype=tf.float64, name="PetalWidth")
         }
 
-        # Recreate the exact same model, including its weights and the optimizer
-        new_model = tf.keras.models.load_model('s3://druid-index-eu-west-1/mlflow/4/0277d7cf6de545f2bd9dc6f2a08bbeb4/artifacts/model/tfmodel')
+        with tf.Session(graph=tf.Graph()) as sess:
+            new_model = tf.saved_model.loader.load(sess, ["serve"], 's3://druid-index-eu-west-1/mlflow/4/0277d7cf6de545f2bd9dc6f2a08bbeb4/artifacts/model/tfmodel')
+            # Recreate the exact same model, including its weights and the optimizer
+            # new_model = tf.keras.models.load_model('s3://druid-index-eu-west-1/mlflow/4/0277d7cf6de545f2bd9dc6f2a08bbeb4/artifacts/model/tfmodel')
 
-        # Show the model architecture
-        print(new_model.summary())
+            # Show the model architecture
+            print(new_model.summary())
 
         receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feat_specifications)
         temp = tempfile.mkdtemp()
