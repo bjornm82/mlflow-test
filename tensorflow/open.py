bb3f155720fccb0aef59090ed2cb4a74a95a1dcc
bjornm82 <bjmooijekind@gmail.com>
2020-01-07 13:38:20 +0100

    Adding tensorflow

--- tensorflow/open.py
+++ tensorflow/open.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
 
-# Recreate the exact same model, including its weights and the optimizer
-new_model = tf.keras.models.load_model('s3://druid-index-eu-west-1/mlflow/4/09c9ce210fec472eb635ee22ea360387/artifacts/model')
 
-# Show the model architecture
-new_model.summary()
+def main(argv):
+    # Recreate the exact same model, including its weights and the optimizer
+    new_model = tf.keras.models.load_model('s3://druid-index-eu-west-1/mlflow/4/09c9ce210fec472eb635ee22ea360387/artifacts/model')
+
+    # Show the model architecture
+    new_model.summary()
+
+if __name__ == '__main__':
+    main(sys.argv)
