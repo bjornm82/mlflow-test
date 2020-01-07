f67e769a11e46045503b4828579b4b77622772fc
bjornm82 <bjmooijekind@gmail.com>
2020-01-07 13:39:08 +0100

    Adding some print

--- tensorflow/open.py
+++ tensorflow/open.py
@@ -6,7 +6,7 @@ def main(argv):
     new_model = tf.keras.models.load_model('s3://druid-index-eu-west-1/mlflow/4/09c9ce210fec472eb635ee22ea360387/artifacts/model')
 
     # Show the model architecture
-    new_model.summary()
+    print(new_model.summary())
 
 if __name__ == '__main__':
     main(sys.argv)
