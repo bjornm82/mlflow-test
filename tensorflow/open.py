387df637695587ab52d18d818eefaf88ad630a38
bjornm82 <bjmooijekind@gmail.com>
2020-01-07 13:36:31 +0100

    Adding load model

---
+++ tensorflow/open.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+# Recreate the exact same model, including its weights and the optimizer
+new_model = tf.keras.models.load_model('s3://druid-index-eu-west-1/mlflow/4/09c9ce210fec472eb635ee22ea360387/artifacts/model')
+
+# Show the model architecture
+new_model.summary()
